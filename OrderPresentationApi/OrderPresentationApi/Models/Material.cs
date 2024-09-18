76cbfd26a9989268d7a83d7597777d6aea294c21
Vinicius Monteiro <vinicius@iconax.com.br>
2024-09-18 14:47:38 -0300

    feat: criação dos modelos para as tabelas Material e TipoMaterial

---
+++ OrderPresentationApi/OrderPresentationApi/Models/Material.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderPresentationApi.Models
+{
+    public class Material
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public int IdTipoMaterial { get; set; }
+
+        public TipoMaterial TipoMaterial { get; set; }
+    }
+}
---
+++ OrderPresentationApi/OrderPresentationApi/Models/TipoMaterial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderPresentationApi.Models
+{
+    public class TipoMaterial
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nome { get; set; }
+
+        public List<Material> Materiais { get; set; }
+    }
+}
