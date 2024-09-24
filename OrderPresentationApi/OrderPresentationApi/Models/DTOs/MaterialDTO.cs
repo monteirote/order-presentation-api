c9acd4b20cc4a7ab98fa951ade3f41482cb142b8
Vinícius Monteiro <vinicius@iconax.com.br>
2024-09-23 21:19:06 -0300

    feat: criação da pasta de DTOs

---
+++ OrderPresentationApi/OrderPresentationApi/Models/DTOs/MaterialDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace OrderPresentationApi.Models.DTOs
+{
+    public class MaterialDTO
+    {
+        public MaterialDTO (Material material) {
+            Id = material.Id;
+            Name = material.Name;
+            IdTipoMaterial = material.IdTipoMaterial;
+            TipoMaterial = material.TipoMaterial.Nome;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int IdTipoMaterial { get; set; }
+        public string TipoMaterial { get; set; }
+    }
+}
